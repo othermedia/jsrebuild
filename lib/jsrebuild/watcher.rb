c833cfed0b326866a5422ada52415d73292fb0be
Benedict Eastaugh <benedict@eastaugh.net>
2011-08-09 17:24:13 +0100

    Document the Watcher class.

--- lib/jsrebuild/watcher.rb
+++ lib/jsrebuild/watcher.rb
@@ -1,7 +1,12 @@
 module JSRebuild
+  # Every jsrebuild process has a Watcher, which keeps tabs on the config,
+  # helper and source files and notifies the application if any of them change.
   class Watcher
     include Eventful
     
+    # Set the interval at which files should be checked for changes, and pass
+    # in a JSRebuild::Config object which tells the watch which config, helper
+    # and source files it should watch.
     def initialize(interval = 0.5, config)
       @interval       = interval
       @config         = config
@@ -11,6 +16,7 @@ module JSRebuild
       @helper_watcher = nil
     end
     
+    # Start the watcher process.
     def start
       return unless @loop.nil?
       
@@ -40,11 +46,17 @@ module JSRebuild
       @loop.run
     end
     
+    # Detach all current source watchers and attach new ones.
+    #
+    # This is a simple and robust way of ensuring that when changes are made
+    # which change which files should be watched, only those files which should
+    # be watched are watched, and no others.
     def reattach_source_watchers
       detach_source_watchers
       attach_source_watchers
     end
     
+    # Attach a file watcher to each source file.
     def attach_source_watchers
       @source_watchers = {}
       
@@ -60,6 +72,7 @@ module JSRebuild
       end
     end
     
+    # Detach all current source watchers.
     def detach_source_watchers
       unless @source_watchers.nil? || @source_watchers.empty?
         @source_watchers.each do |file, watcher|
@@ -68,6 +81,7 @@ module JSRebuild
       end
     end
     
+    # Detach all watchers and stop the event loop.
     def stop
       detach_source_watchers
       
@@ -78,14 +92,23 @@ module JSRebuild
     end
   end
   
+  # The FileWatcher class is a small extension to the Coolio::StatWatcher class
+  # which encapsulates one further piece of information, the last time the
+  # watched file was changed.
+  #
+  # When the watched file changes, FileWatcher objects notify any observers
+  # only when the file has been modified since it was last checked.
   class FileWatcher < Coolio::StatWatcher
     include Eventful
     
+    # Set the watched file's ctime.
     def initialize(path, interval = 0)
       @ctime = File.ctime(path) if File.exists?(path)
       super
     end
     
+    # Notify any observers if either the file no longer exists, or if the file
+    # has been modified since it was last checked.
     def on_change
       fire(:change) and return unless File.exists?(path)
       
